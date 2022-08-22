f38ed9e62cefaee0e2f99baf4d515eb9e9bfe8a0
FSemenas <florenciasemenas@gmail.com>
2022-08-22 17:59:22 -0300

    Cambios 22082022

--- src/rpart/z101_PrimerModelo_ConGridSerchPrimeraClase.R
+++ src/rpart/z101_PrimerModelo_ConGridSerchPrimeraClase.R
@@ -19,9 +19,9 @@ dapply  <- dataset[ foto_mes==202103 ]  #defino donde voy a aplicar el modelo
 modelo  <- rpart(formula=   "clase_ternaria ~ .",  #quiero predecir clase_ternaria a partir de el resto de las variables
                  data=      dtrain,  #los datos donde voy a entrenar
                  xval=      0,
-                 cp=       -0.3,   #esto significa no limitar la complejidad de los splits
-                 minsplit=  0,     #minima cantidad de registros para que se haga el split
-                 minbucket= 1,     #tamaño minimo de una hoja
+                 cp=       -1,   #esto significa no limitar la complejidad de los splits
+                 minsplit=  80,     #minima cantidad de registros para que se haga el split
+                 minbucket= 40,     #tamaño minimo de una hoja
                  maxdepth=  4 )    #profundidad maxima del arbol
 
 
@@ -46,8 +46,8 @@ dapply[ , Predicted := as.numeric( prob_baja2 > 1/40 ) ]
 #genero el archivo para Kaggle
 #primero creo la carpeta donde va el experimento
 dir.create( "./exp/" )
-dir.create( "./exp/KA2001" )
+dir.create( "./exp/KA2003" )
 
 fwrite( dapply[ , list(numero_de_cliente, Predicted) ], #solo los campos para Kaggle
-        file= "./exp/KA2001/K101_001.csv",
+        file= "./exp/KA2003/K101_001.csv",
         sep=  "," )
